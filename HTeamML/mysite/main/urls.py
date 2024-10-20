dea1e4928e08547f67fe86f4a6e0d385e8bdacc2
22stevenwu <22stevenwu@gmail.com>
2024-10-20 19:09:58 -0400

    Added navbar
    created base.html with navbar html and style, and blank actions.html, profile.html, and rewards.html pages; updated urls.py and views.py

--- HTeamML/mysite/main/urls.py
+++ HTeamML/mysite/main/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
   path("", views.home, name="home"),
   path("login/", views.login_view, name="login"),
   path("logout/", views.logout_view, name="logout"),
+  path('actions/', views.actions_view, name='actions'), 
+  path('rewards/', views.rewards_view, name='rewards'),
+  path('profile/', views.rewards_view, name='profile'),
 ]
--- HTeamML/mysite/main/views.py
+++ HTeamML/mysite/main/views.py
@@ -12,3 +12,12 @@ def login_view(request):
 def logout_view(request):
   logout(request)
   return redirect("home")
+
+def actions_view(request):
+  return render(request, 'actions.html')
+
+def rewards_view(request):
+  return render(request, 'rewards.html')
+
+def profile_view(request):
+  return render(request, 'profile.html')
