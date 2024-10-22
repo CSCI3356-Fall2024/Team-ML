4c80cbf3ea90f764ac96472e3282fe32665ab96f
k1chen <chenaun@bc.edu>
2024-10-21 20:43:26 -0400

    added dropdown for graduation year

--- HTeamML/mysite/main/models.py
+++ HTeamML/mysite/main/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 class User(models.Model):
+    gradYearOptions = [(2025, "2025"), (2026, "2026"), (2027, "2027"), (2028, "2028"),]
+
     id = models.AutoField(primary_key=True)
     fullname = models.CharField(max_length=255, blank=False, null=False)
     school = models.CharField(max_length=255, blank=False, null=False)
     email = models.EmailField(max_length=254, blank=False, null=False)
     major = models.CharField(max_length=255, blank=False, null=False)
     minor = models.CharField(max_length=255, default="None", blank=False, null=False)
-    gradyear = models.PositiveIntegerField()
+    gradyear = models.PositiveIntegerField(choices = gradYearOptions, blank=False, null=False)
     profile_completed = models.BooleanField(default=False)
     supervisor = models.BooleanField(default=False)
+    
     
     def __str__(self):
         return self.email
