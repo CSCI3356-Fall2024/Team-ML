7cbece40044ed3b997a6fad813302a4d836fc54a
22stevenwu <22stevenwu@gmail.com>
2024-12-17 12:36:01 -0500

    update build.sh x3

--- build.sh
+++ build.sh
@@ -5,10 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip3 install -r requirements.txt
 
-cd -
-
 # Convert static asset files
-python3 manage.py collectstatic --no-input
+python3 cd .. manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python3 manage.py migrate
+python3 cd .. manage.py migrate
