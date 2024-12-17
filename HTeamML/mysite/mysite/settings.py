c70c6b2a0dae9e427acb399b7aea337570544612
22stevenwu <22stevenwu@gmail.com>
2024-12-17 14:48:02 -0500

    fix pfp x4

--- HTeamML/mysite/mysite/settings.py
+++ HTeamML/mysite/mysite/settings.py
@@ -184,7 +184,7 @@ TIME_ZONE = 'America/New_York'
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://mysite-xrgz.onrender.com/'
--- HTeamML/mysite/mysite/urls.py
+++ HTeamML/mysite/mysite/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("oauth/", include("social_django.urls", namespace= "social")),
     path("", include("main.urls"))
-]
-if settings.MEDIA_URL:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#if settings.DEBUG:
+#    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
