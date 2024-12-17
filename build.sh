e74f35d3b47371071e6ed5806482a08d33767fcb
22stevenwu <22stevenwu@gmail.com>
2024-12-17 12:43:41 -0500

    update build.sh x6

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # Exit on error
 set -o errexit
 
-cd /opt/render/project/src/HTeamML/mysite
-
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip3 install -r requirements.txt
+
+cd /opt/render/project/src/HTeamML/mysite
 
 # Convert static asset files
 python3 manage.py collectstatic --no-input
