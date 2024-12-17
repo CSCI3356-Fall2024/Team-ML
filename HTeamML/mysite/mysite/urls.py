8a56e33a56e4cc9501c9863c0bee4d500cb28a28
22stevenwu <22stevenwu@gmail.com>
2024-12-17 14:35:57 -0500

    fix pfp x3

--- HTeamML/mysite/mysite/urls.py
+++ HTeamML/mysite/mysite/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path("oauth/", include("social_django.urls", namespace= "social")),
     path("", include("main.urls"))
 ]
-if settings.DEBUG:
+if settings.MEDIA_URL:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
