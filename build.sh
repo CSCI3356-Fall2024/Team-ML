9c2a0663aaeb219774fbf9232baae063e8708d1f
22stevenwu <22stevenwu@gmail.com>
2024-12-17 14:02:28 -0500

    create superuser

--- build.sh
+++ build.sh
@@ -11,4 +11,5 @@ cd /opt/render/project/src/HTeamML/mysite
 python3 manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python3 manage.py migrate
+python3 manage.py migrate && python3 manage.py createsuperuser --no-input ||
+echo "Superuser already exists"
