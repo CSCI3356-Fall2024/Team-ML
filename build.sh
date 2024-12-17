4c446436d01adb5310c2692539e7efa000b8d9dd
22stevenwu <22stevenwu@gmail.com>
2024-12-17 12:31:46 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip3 install -r requirements.txt
 
+cd ..
+
 # Convert static asset files
 python3 manage.py collectstatic --no-input
 
